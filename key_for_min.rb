a96cee5bc9ede44faa6ddd84c1d83e09c52b4b88
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-26 17:39:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,11 @@ def key_for_min_value(name_hash)
     nil
   else
     smallest_value = 0
-    smallest_key = ""
+    smallest_key = []
     name_hash.each do |key, value|
       if value < smallest_value
         smallest_value = value
-        smallest_key = key
+        smallest_key << key
       end
     end
     smallest_key
