2d2b3c9befa0a799b8f597efd02816ed8e376302
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-26 17:37:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(name_hash)
     nil
   else
     smallest_value = 0
-
     name_hash.each do |key, value|
-
+      if value < smallest_value
+        smallest_value = value
     end
   end
 end
