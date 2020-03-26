0664e41b1f8982f5386325708b9be472abde39d8
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-26 17:53:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
 
 def key_for_min_value(name_hash)
   if name_hash.size == 0
@@ -19,7 +18,6 @@ def key_for_min_value(name_hash)
         smallest_key << key
       end
     end
-    #puts smallest_value
     smallest_key.last
   end
 end
